43fc0890f80557bfd6ddaa5cd3c58cd3cf93d8e7
Balwinder Singh <v-balwsingh@microsoft.com>
2024-01-02 02:36:27 +0530

    Commit

--- 01BasicComponent/src/app/components/reactiveForm.component.ts
+++ 01BasicComponent/src/app/components/reactiveForm.component.ts
@@ -1,12 +1,47 @@
 import { Component, OnInit } from "@angular/core";
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
+
+
 
 @Component({
     selector: 'app-reactive-form',
     templateUrl: '../components/reactiveForm.component.html'
 
 })
+
 export class ReactiveFormComponent implements OnInit {
     ngOnInit(): void {
 
     }
+
+    userName: FormControl = new FormControl('', [Validators.required]);
+    password: FormControl = new FormControl('', [Validators.required]);
+    loginFormSubmit() {
+        console.log(this.userName.value)
+        console.log(this.password.value)
+    }
+    loginFormGroup = new FormGroup({
+        usename: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required])
+    })
+    loginFormGroupSubmit() {
+        console.log(this.loginFormGroup.controls.usename.value)
+        console.log(this.loginFormGroup.controls.password.value)
+    }
+    //Dynamic form
+    dynamicForm = new FormGroup({
+        username: new FormControl('', []),
+        password: new FormControl('', [])
+    });
+    setDynamicValue() {
+        this.dynamicForm.controls.username.setValue("Updated dynamic username");
+        this.dynamicForm.controls.password.setValue("Updated dynamic password");
+    }
+
+    //Custom Validation
+    customValidationForm = new FormGroup({
+        username: new FormControl('', []),
+        password: new FormControl('')
+    });
+
 }
